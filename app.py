5e9284e684244d40730d4b7e8b9362a21f69ec8e
sachin <sachin.tiwari50@gmail.com>
2021-04-24 14:27:42 +0530

    app name

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def show_tables():
     titles = ['na', 'Report card'])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=7000, host="0.0.0.0")
 
 
 #import pandas as pd
