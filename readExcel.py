e7c2d1cee7f5ade3927937cdc46854f9773c4b4c
sachin <sachin.tiwari50@gmail.com>
2021-04-24 13:18:41 +0530

    first commit

---
+++ readExcel.py
@@ -0,0 +1,30 @@
+
+from flask import *
+import pandas as pd
+import numpy as np
+app = Flask(__name__)
+
+@app.route("/")
+def show_tables():
+    pd.set_option("expand_frame_repr", False)
+    #pd.set_option('max_colwidth', None)
+    df = pd.read_excel(r'sampleData.xlsx',header=1)
+    df.fillna('', inplace=True)
+    df.rename(columns = {'Unnamed: %d'%i : '' for i in range(1,11)}, inplace = True)
+    #print(df)
+
+    #print(clean_df)
+    #data.set_index(['Name'], inplace=True)
+    #data.index.name=None
+    #females = data.loc[data.Gender=='f']
+    #males = data.loc[data.Gender=='m']
+    return render_template('view.html',tables=[df.to_html(classes='female')], #, males.to_html(classes='male')],
+    titles = ['na', 'Report card'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#import pandas as pd
+#data = pd.read_excel('/home/govind/python/Class_11_A_Annual_Exam.xlsx', sheet_name="Sheet2", skiprows=13, nrows=32)
+#print(data.head(1).to_html(classes='result'))
